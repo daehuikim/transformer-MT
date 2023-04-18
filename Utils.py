6ef9c3d37df9714353f4b26ffe4c7ccbe3196a31
daehuikim <andrea0119@postech.ac.kr>
2023-04-18 12:39:40 +0900

    Add trainer codes

--- Utils.py
+++ Utils.py
@@ -61,3 +61,19 @@ def subsequent_mask(size):
     mask = torch.triu(torch.ones(shape), diagonal=1).type(torch.uint8)
     return mask == 0
 
+class DummyOptimizer(torch.optim.Optimizer):
+    def __init__(self):
+        self.param_groups = [{"lr": 0}]
+        None
+
+    def step(self):
+        None
+
+    def zero_grad(self, set_to_none=False):
+        None
+
+
+class DummyScheduler:
+    def step(self):
+        None
+
--- testing/DataGen.py
+++ testing/DataGen.py
@@ -3,7 +3,23 @@ from os.path import exists
 from torchtext.vocab import build_vocab_from_iterator
 import torchtext.datasets as datasets
 import torch
+import spacy
 
+def load_tokenizers():
+
+    try:
+        spacy_de = spacy.load("de_core_news_sm")
+    except IOError:
+        os.system("python -m spacy download de_core_news_sm")
+        spacy_de = spacy.load("de_core_news_sm")
+
+    try:
+        spacy_en = spacy.load("en_core_web_sm")
+    except IOError:
+        os.system("python -m spacy download en_core_web_sm")
+        spacy_en = spacy.load("en_core_web_sm")
+
+    return spacy_de, spacy_en
 
 def tokenize(text, tokenizer):
     return [tok.text for tok in tokenizer.tokenizer(text)]
